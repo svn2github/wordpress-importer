b20adaa26a551e8c430c102b0cdf114d83a5e7fc
duck_ <duck_@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-05 13:26:10 +0000

    Double check the post_type of pre-existing posts before blocking it from being imported as a duplicate, see #WP19218.
    git-svn-id: http://plugins.svn.wordpress.org/wordpress-importer@485059 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/wordpress-importer.php
+++ trunk/wordpress-importer.php
@@ -539,7 +539,7 @@ class WP_Import extends WP_Importer {
 			$post_type_object = get_post_type_object( $post['post_type'] );
 
 			$post_exists = post_exists( $post['post_title'], '', $post['post_date'] );
-			if ( $post_exists ) {
+			if ( $post_exists && get_post_type( $post_exists ) == $post['post_type'] ) {
 				printf( __('%s &#8220;%s&#8221; already exists.', 'wordpress-importer'), $post_type_object->labels->singular_name, esc_html($post['post_title']) );
 				echo '<br />';
 				$comment_post_ID = $post_id = $post_exists;
